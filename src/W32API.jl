3658cfa761e4211ada4be4ea04a01da1948517ff
Hatsune Miku <999Hatsune@gmail.com>
2016-02-14 01:04:41 +0900

    fix Type WARNINGs on auto conversion

--- src/W32API.jl
+++ src/W32API.jl
@@ -16,8 +16,8 @@ function MBoxA(Msg::AbstractString, Title::AbstractString) # expect ASCIIString
   user32 = Base.Libdl.dlopen_e("user32.dll")
   MessageBoxA = Base.Libdl.dlsym(user32, :MessageBoxA)
   err = ccall(MessageBoxA, stdcall,
-    UInt32, (Ptr{UInt32}, Ptr{Cchar}, Ptr{Cchar}, UInt32),
-    0, Msg, Title, 0)
+    UInt, (Ptr{Void}, Ptr{Cchar}, Ptr{Cchar}, UInt),
+    C_NULL, Msg, Title, 0)
   Base.Libdl.dlclose(user32)
   return err
 end
@@ -30,9 +30,8 @@ function MBoxW(Msg::Array{UInt16, 1}, Title::Array{UInt16, 1}) # expect wchar_t
   user32 = Base.Libdl.dlopen_e("user32.dll")
   MessageBoxW = Base.Libdl.dlsym(user32, :MessageBoxW)
   err = ccall(MessageBoxW, stdcall,
-    # UInt32, (Ptr{UInt32}, Ptr{Cwchar_t}, Ptr{Cwchar_t}, UInt32),
-    UInt32, (Ptr{UInt32}, Ptr{UInt16}, Ptr{UInt16}, UInt32),
-    0, Msg, Title, 0)
+    UInt, (Ptr{Void}, Ptr{Cwchar_t}, Ptr{Cwchar_t}, UInt),
+    C_NULL, Msg, Title, 0)
   Base.Libdl.dlclose(user32)
   return err
 end
